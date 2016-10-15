8a4fd96741464d717157f81f6d6c1fd7dfe28c5c
Vaughn Walters <vaughnwalters@gmail.com>
2016-10-15 18:45:28 -0500

    pulling back correct data, not able to correctly parse restaurant strings yet

--- cow.js
+++ cow.js
@@ -1,19 +1,27 @@
+"use strict";
+
 const { get } = require('request');
 const { load } = require('cheerio');
 const [,, city, state] = process.argv;
 const url =  `https://www.happycow.net/searchmap/?kw=&location=${city}+${state}&vegan=true`
 
+const restaurantArr = [];
+const addressArr = [];
+
+
 console.log("city", city);
 console.log("state", state);
 console.log("url", url);
 
+
+
 get(url, (err, res, body) => {
   let $ = load(body);
-  let restaurantList = ($(".info h4")+"\n")
-  let addressList = ($(".venue-location-item")+"\n")
+  let restaurantList = ($(".venue-name").text()+"\n")
+  let addressList = $(".venue-location-item").text();
+  let addressSplit = addressList.split(', USA')
+  addressSplit.pop()
   console.log("restaurantList", restaurantList);
-  console.log("addressList", addressList);
+  console.log("addressSplit", addressSplit);
 })
-
-  // process.stdout.write($(".info h4").text()+"\n")
 
