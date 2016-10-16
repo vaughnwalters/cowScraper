5d5a84cbe7d9920110d1de463129f4f7113a4a6a
Vaughn Walters <vaughnwalters@gmail.com>
2016-10-15 21:32:49 -0500

    concat name with address correctly

--- cow.js
+++ cow.js
@@ -5,23 +5,37 @@ const { load } = require('cheerio');
 const [,, city, state] = process.argv;
 const url =  `https://www.happycow.net/searchmap/?kw=&location=${city}+${state}&vegan=true`
 
-const restaurantArr = [];
-const addressArr = [];
-
-
 console.log("city", city);
 console.log("state", state);
-console.log("url", url);
+// console.log("url", url);
 
 
 
 get(url, (err, res, body) => {
   let $ = load(body);
-  let restaurantList = ($(".venue-name").text()+"\n")
-  let addressList = $(".venue-location-item").text();
-  let addressSplit = addressList.split(', USA')
-  addressSplit.pop()
-  console.log("restaurantList", restaurantList);
-  console.log("addressSplit", addressSplit);
-})
+  
+  let restaurantList = ($(".venue-name")+"")
+  restaurantList = restaurantList.replace(/<\/h5>/g, "")
+  let restNameArr = restaurantList.split('<h5 class="group inner list-group-item-heading venue-name">');
+  restNameArr.shift();
 
+  let addressList = $(".venue-location-item").text();
+  let restAddressArr = addressList.split(', USA');
+  restAddressArr.pop();
+  
+  // console.log("restNameArr", restNameArr);
+  // console.log("restAddressArr", restAddressArr);
+
+  let restaurantArr;
+  if (restAddressArr.length === restNameArr.length) {
+    for (let i = 0; i < restAddressArr.length; i++) {
+        let restInfo = restNameArr[i] + ", " + restAddressArr[i];
+        restInfo = restInfo.replace(/&apos;/g, "'");
+        process.stdout.write(restInfo+"\n");
+      }
+  } else if (restAddressArr.length > restNameArr.length) {
+    console.log("more restaurant addresses than names.  problem on happy cow's end.");
+  } else if (restAddressArr.length < restNameArr.length) {
+    console.log("more restaurant names than addresses.  problem on happy cow's end."); 
+  }
+});
